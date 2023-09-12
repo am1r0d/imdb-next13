a619c647e02a1d126b81f2edfdb0115ff8fa5f68
Amir 0d <amirmsut@gmail.com>
2023-09-12 18:34:58 +0330

    Add Dark mode

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
-import MenuItem from "./MenuItem";
+import Link from "next/link";
 import { AiFillHome } from "react-icons/ai";
 import { BsFillInfoCircleFill } from "react-icons/bs";
-import Link from "next/link";
+
+// Component
+import MenuItem from "./MenuItem";
+import DarkModeSwitch from "./DarkModeSwitch";
+
 const Header = () => {
     return (
-        // <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
         <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
             <div className="flex">
                 <MenuItem title="Home" address="/" Icon={AiFillHome} />
@@ -16,6 +19,7 @@ const Header = () => {
                 />
             </div>
             <div className="flex items-center space-x-5">
+                <DarkModeSwitch />
                 <Link href="/">
                     <h2 className="text-2xl">
                         <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
