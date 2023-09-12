393bc4571097e1d4b4153fb0430fd2310e01b81d
Amir 0d <amirmsut@gmail.com>
2023-09-12 18:40:51 +0330

    update provider, update Header add shadow and Sticky navbar

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -6,7 +6,7 @@ const Providers = ({ children }) => {
     return (
         <div>
             <ThemeProvider enableSystem={true} attribute="class">
-                <div className="dark:bg-gray-700 dark:text-gray-200 text-gray-700 transition-colors duration-300 min-h-screen">
+                <div className="dark:bg-gray-800 dark:text-gray-200 text-gray-900 transition-colors duration-300 min-h-screen">
                     {children}
                 </div>
             </ThemeProvider>
